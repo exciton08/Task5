2fda377b98f18360b9ed47542e5e1160b1e6f584
Mergen <exciton08@mail.ru>
2022-11-14 07:05:27 +0300

    Выполнено задание к билету №5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача к билету №5:" +
+                "\n" +
+                "Дан массив пар названий книг и авторов, необходимо привести каждую пару в строку вида: «Название книги» И.О.Автора");
+
+        System.out.println();
+        System.out.println("Решение:");
+        String[] namesOfBooks = {"The Count of Monte Cristo", "Love of live"};
+        String[] authors = {"Alexandre Dumas", "Jack London"};
+        for (int i = 0; i < namesOfBooks.length; i++) {
+            System.out.println("«" + namesOfBooks[i] + "»" + " by " + authors[i]);
+        }
     }
 }
